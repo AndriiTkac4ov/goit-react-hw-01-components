a8b8c2dd13e7499f9b26d685cffa73bad9fd20fe
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2022-12-29 17:44:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,11 +15,6 @@ export const App = () => {
     <div
       style={{
         padding: 40,
-        // textAlign: 'center',
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
